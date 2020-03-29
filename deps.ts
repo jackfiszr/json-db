10fa2324ce6021865ab559972629c1f5f930b876
Jacek Fiszer <jackfiszr+gh@gmail.com>
2020-03-29 17:47:33 +0200

    deps update

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 const { open, remove, truncate, readdir, readFile, stat, writeFile, close, mkdir } = Deno
-export { join } from 'https://deno.land/std@v0.36.0/path/mod.ts'
+export { join } from 'https://deno.land/std@v0.38.0/path/mod.ts'
 export {
   open,
   remove as unlink,
--- test/test_deps.ts
+++ test/test_deps.ts
@@ -1,5 +1,5 @@
 export { faker } from 'https://raw.githubusercontent.com/jackfiszr/faker/master/mod.ts'
-export { assertEquals } from 'https://deno.land/std@v0.36.0/testing/asserts.ts'
+export { assertEquals } from 'https://deno.land/std@v0.38.0/testing/asserts.ts'
 import { __ } from 'https://deno.land/x/dirname/mod.ts'
 export const { __filename, __dirname } = __(import.meta)
 export const { test } = Deno
